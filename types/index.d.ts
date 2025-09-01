540d7d108f621160a2a04a76637ac588c1ec6ccc
Dharmesh Mishra <dharmeshmishra@Dharmeshs-MacBook-Air.local>
2025-09-01 07:26:01 +0530

    implemented auth and storage

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,51 +1,51 @@
-// interface Feedback {
-//   id: string;
-//   interviewId: string;
-//   totalScore: number;
-//   categoryScores: Array<{
-//     name: string;
-//     score: number;
-//     comment: string;
-//   }>;
-//   strengths: string[];
-//   areasForImprovement: string[];
-//   finalAssessment: string;
-//   createdAt: string;
-// }
-//
-// interface Interview {
-//   id: string;
-//   role: string;
-//   level: string;
-//   questions: string[];
-//   techstack: string[];
-//   createdAt: string;
-//   userId: string;
-//   type: string;
-//   finalized: boolean;
-// }
-//
-// interface CreateFeedbackParams {
-//   interviewId: string;
-//   userId: string;
-//   transcript: { role: string; content: string }[];
-//   feedbackId?: string;
-// }
-//
-// interface User {
-//   name: string;
-//   email: string;
-//   id: string;
-// }
-//
-// interface InterviewCardProps {
-//   interviewId?: string;
-//   userId?: string;
-//   role: string;
-//   type: string;
-//   techstack: string[];
-//   createdAt?: string;
-// }
+interface Feedback {
+  id: string;
+  interviewId: string;
+  totalScore: number;
+  categoryScores: Array<{
+    name: string;
+    score: number;
+    comment: string;
+  }>;
+  strengths: string[];
+  areasForImprovement: string[];
+  finalAssessment: string;
+  createdAt: string;
+}
+
+interface Interview {
+  id: string;
+  role: string;
+  level: string;
+  questions: string[];
+  techstack: string[];
+  createdAt: string;
+  userId: string;
+  type: string;
+  finalized: boolean;
+}
+
+interface CreateFeedbackParams {
+  interviewId: string;
+  userId: string;
+  transcript: { role: string; content: string }[];
+  feedbackId?: string;
+}
+
+interface User {
+  name: string;
+  email: string;
+  id: string;
+}
+
+interface InterviewCardProps {
+  interviewId?: string;
+  userId?: string;
+  role: string;
+  type: string;
+  techstack: string[];
+  createdAt?: string;
+}
 //
 // interface AgentProps {
 //   userName: string;
@@ -71,17 +71,17 @@
 //   limit?: number;
 // }
 //
-// interface SignInParams {
-//   email: string;
-//   idToken: string;
-// }
-//
-// interface SignUpParams {
-//   uid: string;
-//   name: string;
-//   email: string;
-//   password: string;
-// }
+interface SignInParams {
+  email: string;
+  idToken: string;
+}
+
+interface SignUpParams {
+  uid: string;
+  name: string;
+  email: string;
+  password: string;
+}
 //
 // type FormType = "sign-in" | "sign-up";
 //
